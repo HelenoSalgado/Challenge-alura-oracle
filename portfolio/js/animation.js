589d9f952156cf31b7d1d83298611cc633146d34
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-06-21 21:21:48 -0300

    :octocat:  Atualizando portifolio
    Processo em 60%.

--- portfolio/js/animation.js
+++ portfolio/js/animation.js
@@ -1,7 +1,9 @@
-var desenho = document.querySelector('body');
+var desenho = document.querySelector('header');
 var hamburguer = document.querySelector('.menu-hamburguer'); 
 var botoes = document.querySelectorAll('button');
-var zoomImg = document.querySelectorAll('.card img');
+var icone = document.querySelectorAll('button i');
+var zoomImg = document.querySelectorAll('.card-capa');
+var previaImg = document.querySelectorAll('.previa-img img');
 var textFocus = document.querySelectorAll('.card-text');
 
 hamburguer.addEventListener('click', () =>{
@@ -12,45 +14,56 @@ zoomImg.forEach( (img) => {
   img.addEventListener('mouseover', (event) =>{
       img.classList.add("zoom");
       setTimeout(() => {
-        event.target.parentNode.classList.add('card-text-visible');
-      }, 200);
-         
+        event.target.parentNode.classList.add('visible');
+      }, 200); 
   })
   img.addEventListener('mouseout', (event) =>{
     img.classList.remove("zoom");
-    event.target.parentNode.classList.remove('card-text-visible');
+    event.target.parentNode.classList.remove('visible');
   })  
 
 })
 
-// desenho.addEventListener('mousemove', (event) => {
-//       var x = event.clientX;
-//       var y = event.clientY;
+desenho.addEventListener('mousemove', (event) => {
+      var x = event.clientX;
+      var y = event.clientY;
   
-//       console.log(x + ", " + y);
+      console.log(x + ", " + y);
 
-//       var cursor = document.createElement('span');
-//       cursor.classList.add('cursor');
-//       cursor.style.top = y + (15) + 'px';
-//       cursor.style.left = x + (15) + 'px';
-//       desenho.appendChild(cursor);
+      var cursor = document.createElement('span');
+      cursor.classList.add('cursor');
+      cursor.style.top = y + (15) + 'px';
+      cursor.style.left = x + (15) + 'px';
+      desenho.appendChild(cursor);
    
-//       setTimeout(() => {
-//            cursor.remove();
-//       }, 500); 
-// })
-var icone = document.querySelector('button i');
-botoes.forEach( (button) => { 
-  
-  button.addEventListener('mouseover', () =>{
-      icone.classList.add("inclina-icone");  
-  })
-  button.addEventListener('mouseout', () =>{
-      icone.classList.remove("inclina-icone");
-  })  
-
+      setTimeout(() => {
+           cursor.remove();
+      }, 500); 
 })
-console.log(botoes);
+
+botoes.forEach( (button) => { 
+  button.addEventListener('mouseover', () =>{
+       icone.forEach((icone) => {
+       icone.classList.add("inclina-icone");
+      
+       button.addEventListener('mouseout', () =>{
+       icone.classList.remove("inclina-icone");
+      })  
+    });    
+  })
+})
+
+img.forEach( (previa) => { 
+  img.addEventListener('mouseover', () =>{
+       previa.forEach((previa) => {
+       previa.style.opacity = "10";
+      
+       img.addEventListener('mouseout', () =>{
+       previa.style.opacity = "10";
+      })  
+    });    
+  })
+})
 
 
  
