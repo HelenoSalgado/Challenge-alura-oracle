c47e91b562d32e75684ecc633f792c3cae31f75b
Heleno Salgado <73316162+HelenoSalgado@users.noreply.github.com>
2022-06-22 21:15:30 -0300

    :up:

--- portfolio/js/animation.js
+++ portfolio/js/animation.js
@@ -1,45 +1,95 @@
-var desenho = document.querySelector('header');
 var hamburguer = document.querySelector('.menu-hamburguer'); 
 var botoes = document.querySelectorAll('button');
 var icone = document.querySelectorAll('button i');
 var zoomImg = document.querySelectorAll('.card-capa');
 var previaImg = document.querySelectorAll('.previa-img img');
 var textFocus = document.querySelectorAll('.card-text');
+var desenho = document.querySelector('body');
 
 hamburguer.addEventListener('click', () =>{
-  document.querySelector('.container').classList.toggle('show-menu');
-})
+  var menuAtivo = document.querySelector('.container').classList.toggle('show-menu');
+  menuAtivo;
 
-zoomImg.forEach( (img) => {
-  img.addEventListener('mouseover', (event) =>{
-      img.classList.add("zoom");
-      setTimeout(() => {
-        event.target.parentNode.classList.add('visible');
-      }, 200); 
-  })
-  img.addEventListener('mouseout', (event) =>{
-    img.classList.remove("zoom");
-    event.target.parentNode.classList.remove('visible');
-  })  
+  if(menuAtivo){
+    desenho.addEventListener('mousemove', sinalMouse);
+  }else{
+    desenho.removeEventListener('mousemove', sinalMouse);
+  }
 
 })
 
-desenho.addEventListener('mousemove', (event) => {
-      var x = event.clientX;
-      var y = event.clientY;
+function sinalMouse(event){
+  var cursor = document.createElement('span');
+  cursor.classList.add('cursor');
+  desenho.appendChild(cursor);
+
+  var x = event.clientX;
+  var y = event.clientY; 
+  cursor.style.top = y + (15) + 'px';
+  cursor.style.left = x + (15) + 'px';
+
+  setTimeout(() => {
+       cursor.remove();
+  }, 500); 
+}
+
+
+
+
+
+
+// function sinalMouseOn(){
   
-      console.log(x + ", " + y);
+//     desenho.addEventListener('mousemove', (event) => {
+//     var x = event.clientX;
+//     var y = event.clientY;
 
-      var cursor = document.createElement('span');
-      cursor.classList.add('cursor');
-      cursor.style.top = y + (15) + 'px';
-      cursor.style.left = x + (15) + 'px';
-      desenho.appendChild(cursor);
-   
-      setTimeout(() => {
-           cursor.remove();
-      }, 500); 
-})
+//     console.log(x + ", " + y);
+
+//     var cursor = document.createElement('span');
+//     cursor.classList.add('cursor');
+//     cursor.style.top = y + (15) + 'px';
+//     cursor.style.left = x + (15) + 'px';
+//     desenho.appendChild(cursor);
+ 
+//     setTimeout(() => {
+//          cursor.remove();
+//     }, 500); 
+//   })
+// }
+// function sinalMouseOff(){
+//   desenho.addEventListener('mousemove', (event) => {
+//     var x = event.clientX;
+//     var y = event.clientY;
+
+//     console.log(x + ", " + y);
+
+//     var cursor = document.createElement('span');
+//     cursor.classList.remove('cursor');
+//     cursor.style.top = y + (15) + 'px';
+//     cursor.style.left = x + (15) + 'px';
+//     desenho.appendChild(cursor);
+ 
+//     setTimeout(() => {
+//          cursor.remove();
+//     }, 500); 
+//   })
+// }
+
+
+// zoomImg.forEach( (img) => {
+//   img.addEventListener('mouseover', (event) =>{
+//       img.classList.add("zoom");
+//       setTimeout(() => {
+//         event.target.parentNode.classList.add('visible');
+//       }, 200); 
+//   })
+//   img.addEventListener('mouseout', (event) =>{
+//     img.classList.remove("zoom");
+//     event.target.parentNode.classList.remove('visible');
+//   })  
+
+// })
 
 botoes.forEach( (button) => { 
   button.addEventListener('mouseover', () =>{
